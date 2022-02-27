e8cc1dfbe4effad7e6e280472ffc44f240e5c44e
JLBegin <ludovick.begin@gmail.com>
2022-02-27 15:31:30 -0500

    fix ray direction not taking into account initial sensor orientation

--- sensorsim/light/uniformRaySource.py
+++ sensorsim/light/uniformRaySource.py
@@ -8,9 +8,10 @@ from pytissueoptics.scene.intersection import Ray
 
 
 class UniformRaySource:
-    def __init__(self, position, direction, xTheta: float, yTheta: float, xResolution: int, yResolution: int):
+    def __init__(self, position: Vector, direction: Vector, xTheta: float, yTheta: float, xResolution: int, yResolution: int):
         self._position = position
         self._direction = direction
+        self._direction.normalize()
         self._theta = (xTheta * np.pi / 180, yTheta * np.pi / 180)
         self._resolution = (xResolution, yResolution)
         self._rays = []
@@ -31,9 +32,9 @@ class UniformRaySource:
     def _createRayAt(self, xTheta: float, yTheta: float):
         self._rays.append(Ray(self._position, self._getRayDirectionAt(xTheta, yTheta)))
 
-    @staticmethod
-    def _getRayDirectionAt(xTheta, yTheta) -> Vector:
-        rayDirection = Vector(-math.sin(xTheta), 0, -math.cos(xTheta))
-        rayDirection.add(Vector(0, math.tan(yTheta), 0))
+    def _getRayDirectionAt(self, xTheta, yTheta) -> Vector:
+        xTheta += math.asin(-self._direction.x)
+        yTheta += math.asin(self._direction.y)
+        rayDirection = Vector(-math.sin(xTheta), math.tan(yTheta), -math.cos(xTheta))
         rayDirection.normalize()
         return rayDirection
